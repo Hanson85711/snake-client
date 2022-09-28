61aedf6a70c893b1fceb6f4d8beb8d8312ef47dd
Hanson Lee <leehanson@alumni.ubc.ca>
2022-09-28 21:24:42 +0000

    play.js connect function connects to host server and returns host server data and tells us when connected + writes our initials to server. client.js imports the exported connect function from play.js

---
+++ client.js
@@ -0,0 +1,2 @@
+const playObj = require("./play");
+
---
+++ play.js
@@ -0,0 +1,30 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243", // IP address here,
+    port: "50541"// PORT number here,
+  });
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: HL");
+  })
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("data", (data) => {
+    console.log(data);
+  });
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
+
+module.exports = {
+  connect,
+};
