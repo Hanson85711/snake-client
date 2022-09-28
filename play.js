f49fd46446f32ecc6ff6a3d24643eb654ffea17d
Hanson Lee <leehanson@alumni.ubc.ca>
2022-09-28 21:57:27 +0000

    set up userinput so whenever user presses ctrl + c, it exits the process

--- play.js
+++ play.js
@@ -1,5 +1,24 @@
 const net = require("net");
 
+// setup interface to handle user input from stdin
+
+const handleUserInput = function (key) {
+  // your code here
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
@@ -27,6 +46,7 @@ const connect = function () {
 
 console.log("Connecting ...");
 connect();
+setupInput();
 
 module.exports = {
   connect,
