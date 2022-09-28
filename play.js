15d987f992281128073b9645485555ca25b9bb4d
Hanson Lee <leehanson@alumni.ubc.ca>
2022-09-28 22:24:31 +0000

    input.js now takes in input from user's keyboard (wasd) for movement. play.js updated to pass in connection object for input.js to send signal to server

--- play.js
+++ play.js
@@ -1,10 +1,11 @@
 const net = require("net");
 const { connect } = require("./client");
 const { setupInput } = require("./input");
+let connectObj;
 
 console.log("Connecting ...");
-connect();
-setupInput();
+connectObj = connect();
+setupInput(connectObj);
 
 
 
