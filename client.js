be1e4f42599c9a51b36b5b4434d68c4080c4ecbb
Hanson Lee <leehanson@alumni.ubc.ca>
2022-09-28 22:07:26 +0000

    refactored files so play imports connect function from client.js and input function from input.js

--- client.js
+++ client.js
@@ -1,2 +1,30 @@
-const playObj = require("./play");
+const net = require("net");
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243", // IP address here,
+    port: "50541"// PORT number here,
+  });
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: HL");
+    // setInterval(() => {
+    //   conn.write("Move: up");
+    // }, 1000);
+  })
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("data", (data) => {
+    console.log(data);
+  });
+
+  return conn;
+};
+
+module.exports = {
+  connect,
+}
 
--- play.js
+++ play.js
@@ -1,53 +1,10 @@
 const net = require("net");
-
-// setup interface to handle user input from stdin
-
-const handleUserInput = function (key) {
-  // your code here
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "165.227.47.243", // IP address here,
-    port: "50541"// PORT number here,
-  });
-
-  conn.on("connect", () => {
-    console.log("Successfully connected to game server");
-    conn.write("Name: HL");
-    // setInterval(() => {
-    //   conn.write("Move: up");
-    // }, 1000);
-  })
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("data", (data) => {
-    console.log(data);
-  });
-
-  return conn;
-};
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 connect();
 setupInput();
 
-module.exports = {
-  connect,
-};
+
+
